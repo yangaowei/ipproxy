82d5cc936bc25b42f835bd0dd1d0cea63623ce60
yangaowei <gaoweiyan@sohu-inc.com>
2017-08-12 10:12:40 +0800

    修改helper内容

--- crawl/helper.go
+++ crawl/helper.go
@@ -13,7 +13,7 @@ type Spider interface {
 
 var (
 	ListIpProxy []*IpProxy
-	IpType      = map[int]string{1: "透明", 2: "匿名", 3: "高匿"}
+	IpType      = map[string]int{"透明": 1, "匿名": 2, "高匿": 3}
 )
 
 type IpProxy struct {
